c26208ad31f211f63ac71b10c8702474c17cce63
RomanNestorko <104514561+RomanXz92@users.noreply.github.com>
2023-06-01 16:24:36 +0300

    finish auth

--- src/controllers/recipe-controller.js
+++ src/controllers/recipe-controller.js
@@ -3,7 +3,13 @@ const { HttpError } = require("../helpers")
 const { ctrlWrapper } = require("../utils")
 
 const getAllRecipe = async (req, res) => {
-  const result = await Recipe.find()
+  const { _id: owner } = req.user
+  const { page = 1, limit = 10 } = req.query
+  const skip = (page - 1) * limit
+  const result = await Recipe.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email")
 
   res.json(result)
 }
--- src/middleWares/upload.js
+++ src/middleWares/upload.js
@@ -1,18 +1,29 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer")
+const path = require("path")
+const { HttpError } = require("../helpers")
 
-const tempDir = path.resolve('temp');
+const tempDir = path.resolve("src/temp")
 
 const multerConfig = multer.diskStorage({
   destination: tempDir,
   filename: (req, file, cb) => {
-    const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, `${uniquePrefix}_${file.originalname}`);
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9)
+    cb(null, `${uniquePrefix}_${file.originalname}`)
   },
-});
+})
 
 const upload = multer({
   storage: multerConfig,
-});
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: (req, file, cb) => {
+    const extention = file.originalname.split(".").pop()
+    if (extention !== "jpg" && extention !== "png" && extention !== "jpeg") {
+      cb(HttpError(400, "File format not allow"))
+    }
+    cb(null, true)
+  },
+})
 
-module.exports = upload;
+module.exports = upload
