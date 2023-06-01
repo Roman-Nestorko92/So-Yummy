7b62466fa22cbc8fff6d0d81475770b3ec386224
VickyBondarenko <quikyv@gmail.com>
2023-06-01 16:12:49 +0200

    Add controller

---
+++ src/models/category.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../utils");
+
+const categorySchema = new Schema({
+  categories: [String],
+});
+categorySchema.post("save", handleMongooseError);
+
+const Category = model("category", categorySchema);
+
+module.exports = Category;
