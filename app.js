1adf95bf404fb76b11428a57918d1864f65d7845
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-06-06 00:30:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use("/api", subsRoutes);
 app.use("/api/ownRecipes", ownRecipesRoutes);
 
 // DOCUMENTATION
-const { optimizeBody } = require("./src/middleWares");
-app.use("/api", optimizeBody, docRoutes);
+
+app.use("/api", docRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
