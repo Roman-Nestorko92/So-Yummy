89b0db068143ebf9a3a674d3c802c2a56eee4e57
VickyBondarenko <quikyv@gmail.com>
2023-06-12 21:22:48 +0200

    Add user-id

--- src/models/category.js
+++ src/models/category.js
@@ -2,9 +2,7 @@ const { Schema, model } = require("mongoose");
 const { handleMongooseError } = require("../utils");
 
 const categorySchema = new Schema({
-
   categories: String,
-
 });
 categorySchema.post("save", handleMongooseError);
 
